05e7dee52f1f2db8564fe25fba1c57584bb3c62c
Lucas Siano <sianolucasdev@hotmail.com>
2024-06-08 18:17:27 -0300

    Ejercicios de colecciones y datos compuestos

---
+++ 3_colecciones_y_datos_compuestos/ejercicio_3.py
@@ -0,0 +1,9 @@
+#  Dada la siguiente lista de frutas [“banana”, “manzana”, “pera”], permitir al usuario ingresar 3
+#  frutas más para agregarlas al final de lista. Luego, mostrar por pantalla la lista completa y
+#  debajo la misma lista pero sólo con las frutas que añadió el usuario.
+
+lista_frutas = ["banana", "manaza", "pera"]
+
+frutas = input("Ingrese 3 frutas separadas por coma: \n ")
+lista_frutas = frutas.split(",")
+
